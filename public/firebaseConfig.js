f275e5672e190582c6c34f869d59cba55ed144bf
Vonnay <jervonaguillera16@gmail.com>
2025-04-14 21:28:17 -0700

    hiding sensitive information

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTzMzj-WjYucvSuPFOul-53gStIfWiWZg",
+  authDomain: "nogaka-bd091.firebaseapp.com",
+  projectId: "nogaka-bd091",
+  storageBucket: "nogaka-bd091.firebasestorage.app",
+  messagingSenderId: "910853337142",
+  appId: "1:910853337142:web:a38f0109f2d5012529e897"
+};
+
+export default firebaseConfig;
